b38090b2914e9e80df52596fb35e451afd1d7312
Jesse Vincent <jesse@bestpractical.com>
2006-08-18 23:15:41 +0000

    * No longer complain on cache expirey.

--- lib/Cache/Simple/TimedExpiry.pm
+++ lib/Cache/Simple/TimedExpiry.pm
@@ -88,11 +88,11 @@ at or after EXPIRYTIME.
 
 sub store ($$$) {
   my ($self,$key,$value) = @_;
-
   my $time = time;
   # Only expire 
   $self->expire($time) if ($time > $self->[3]);
 
+  return undef unless ($key);
   $self->[1]->{$key} = $value;
 
     push @{$self->[2]}, [ time, $key ];
